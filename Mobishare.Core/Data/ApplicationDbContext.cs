28dad3359d46f1152ad4b51566595643b98fd133
Daniele-cosimo <cosimodaniele70@gmail.com>
2025-03-24 23:41:38 +0100

    Update ApplicationDbContext

--- Mobishare.Core/Data/ApplicationDbContext.cs
+++ Mobishare.Core/Data/ApplicationDbContext.cs
@@ -1,13 +1,23 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using Mobishare.Core.Models.User;
+using Mobishare.Core.Models.Maps;
+using Mobishare.Core.Models.Users;
+using Mobishare.Core.Models.Vehicles;
 
 namespace Mobishare.Core.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
-        public DbSet<Balances> Balances { get; set; }
+        public DbSet<Balance> Balance { get; set; }
         public DbSet<Feedback> Feedback { get; set; }
+        public DbSet<City> City { get; set; }
+        public DbSet<ParkingSlot> ParkingSlot { get; set; }
+        public DbSet<Vehicle> Vehicle { get; set; }
+        public DbSet<Repair> Repair { get; set; }
+        public DbSet<Report> Report { get; set; }
+        public DbSet<Ride> Ride { get; set; }
+        public DbSet<RepairAssignment> RepairAssignment { get; set; }
+
     }
 }
