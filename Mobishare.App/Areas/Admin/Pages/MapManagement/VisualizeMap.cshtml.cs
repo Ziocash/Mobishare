4fee8d8dfc77fa4c61a2674ca5fdba299dcc7e25
BeastOfShadow <simone.negro.2002@gmail.com>
2025-04-19 19:19:53 +0200

    Added map management page with subpages

---
+++ Mobishare.App/Areas/Admin/Pages/MapManagement/VisualizeMap.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Mobishare.App.Areas.Admin.Pages.MapManagement
+{
+    public class VisualizeMapModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
