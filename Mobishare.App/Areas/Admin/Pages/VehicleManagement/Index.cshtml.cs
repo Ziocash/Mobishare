07c45b1fb752975d8e3080d8034b922f7bbfd835
BeastOfShadow <simone.negro.2002@gmail.com>
2025-04-19 19:22:49 +0200

    Added vehicle management page with subpages

---
+++ Mobishare.App/Areas/Admin/Pages/VehicleManagement/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Mobishare.App.Areas.Admin.Pages.VehicleManagement
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
