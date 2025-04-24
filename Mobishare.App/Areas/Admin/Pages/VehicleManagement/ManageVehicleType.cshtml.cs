e2c478a307787dedc68d74890065e769c9e6748c
BeastOfShadow <simone.negro.2002@gmail.com>
2025-04-24 11:25:46 +0200

    Implemeted crud logic

--- Mobishare.App/Areas/Admin/Pages/VehicleManagement/ManageVehicleType.cshtml.cs
+++ Mobishare.App/Areas/Admin/Pages/VehicleManagement/ManageVehicleType.cshtml.cs
@@ -1,12 +1,124 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using AutoMapper;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Mobishare.Core.Models.Vehicles;
+using Mobishare.Core.Requests.Vehicles.VehicleTypeRequests.Commands;
+using Mobishare.Core.Requests.Vehicles.VehicleTypeRequests.Queries;
+using Mobishare.Core.VehicleClassification;
 
 namespace Mobishare.App.Areas.Admin.Pages.VehicleManagement
 {
     public class ManageVehicleTypeModel : PageModel
     {
-        public void OnGet()
+        private readonly ILogger<ManageVehicleTypeModel> _logger;
+        private readonly IMediator _mediator;
+        private readonly IMapper _mapper;
+        public IEnumerable<VehicleType> AllVehicleTypes { get; set; }
+
+        public ManageVehicleTypeModel(ILogger<ManageVehicleTypeModel> logger, IMediator mediator, IMapper mapper)
         {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        [BindProperty]
+        public InputSportModel Input { get; set; }
+
+        /// <summary>
+        /// Model for the input form to add a new vehicle type.
+        /// Model name is required and must be unique.
+        /// Vehicle type is required and must be one of the defined vehicle types.
+        /// Price per minute is required and must be greater than 0.
+        /// </summary>
+        public class InputSportModel
+        {
+            [Required(ErrorMessage = "Provide a vehicle model.")]
+            [MaxLength(100, ErrorMessage = "Model name cannot be longer than 50 characters.")]
+            public string Model { get; set; }
+            [Required(ErrorMessage = "Select a valid vehicle type.")]
+            public VehicleTypes? Type { get; set; }
+            [Required(ErrorMessage = "Provide a value.")]
+            [Range(0.01, double.MaxValue, ErrorMessage = "Price per minute must be greater than 0")]
+            public decimal? PricePerMinute { get; set; }
+        }
+
+        public async Task<IActionResult> OnPostAddVehicleType()
+        {
+            if (!ModelState.IsValid)
+            {
+                AllVehicleTypes = await _mediator.Send(new GetAllVehicleType());
+
+                _logger.LogWarning("Invalid model states. Model states status: " + !ModelState.IsValid);
+
+                return Page();
+            }
+
+            // TO-DO: Check if the vehicle type already exists in the database with the same model name.
+
+            await _mediator.Send(_mapper.Map<CreateVehicleType>(
+                new VehicleType
+                {
+                    Model = Input.Model,
+                    Type = Input.Type.ToString()!,
+                    PricePerMinute = Input.PricePerMinute ?? 0.0m,
+                    CreatedAt = DateTime.UtcNow
+                }));
+
+            return RedirectToPage();
+        }
+
+        public async Task<IActionResult> OnPostUpdateVehicleType(int id)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                AllVehicleTypes = await _mediator.Send(new GetAllVehicleType());
+
+                _logger.LogWarning("Invalid model states. Model states status: " + !ModelState.IsValid);
+
+                return Page();
+            }
+
+            var rawPriceValue = ModelState["Input.PricePerMinute"]?.AttemptedValue;
+
+            await _mediator.Send(_mapper.Map<UpdateVehicleType>(
+                new VehicleType
+                {
+                    Id = id,
+                    Model = Input.Model,
+                    Type = Input.Type.ToString()!,
+                    PricePerMinute = decimal.Parse(rawPriceValue),
+                    CreatedAt = DateTime.UtcNow
+                }
+            ));
+
+            var aus = Decimal.Parse(rawPriceValue);
+
+            return RedirectToPage();
+        }
+
+        public async Task<IActionResult> OnPostDeleteVehicleType(int id)
+        {
+            await _mediator.Send(_mapper.Map<DeleteVehicleType>(
+                new VehicleType
+                {
+                    Id = id
+                }));
+
+            _logger.LogInformation("Vehicle Type succesflully deleted.");
+
+            AllVehicleTypes = await _mediator.Send(new GetAllVehicleType());
+
+            return Page();
+        }
+
+        public async Task OnGet()
+        {
+            AllVehicleTypes = await _mediator.Send(new GetAllVehicleType());
         }
     }
 }
