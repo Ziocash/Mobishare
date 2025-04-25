9cda6a42fc2df91e8d6227d654e2299d8a65bbe8
BeastOfShadow <simone.negro.2002@gmail.com>
2025-04-25 23:51:03 +0200

    Setup for vheicle

--- Mobishare.App/Areas/Admin/Pages/VehicleManagement/ManageVehicle.cshtml.cs
+++ Mobishare.App/Areas/Admin/Pages/VehicleManagement/ManageVehicle.cshtml.cs
@@ -1,10 +1,37 @@
+using System.ComponentModel.DataAnnotations;
+using AutoMapper;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Mobishare.Core.Models.Vehicles;
 
 namespace Mobishare.App.Areas.Admin.Pages.VehicleManagement
 {
     public class ManageVehicleModel : PageModel
     {
+        private readonly ILogger<ManageVehicleModel> _logger;
+        private readonly IMediator _mediator;
+        private readonly IMapper _mapper;
+        public IEnumerable<Vehicle> AllVehicles { get; set; }
+        
+        public ManageVehicleModel(ILogger<ManageVehicleModel> logger, IMediator mediator, IMapper mapper)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        [BindProperty]
+        public InputVehicleModel Input { get; set; }
+
+        /// <summary>
+        /// Model for the input form to add a new vehicle.
+        /// <summary>
+        public class InputVehicleModel
+        {
+            // public int Id { get; set; }
+        }
+        
         public void OnGet()
         {
         }
