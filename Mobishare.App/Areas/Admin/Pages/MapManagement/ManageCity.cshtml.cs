6a3cbdbaf4ecc16516e496b7807e9ce5ad46e50e
BeastOfShadow <simone.negro.2002@gmail.com>
2025-04-25 12:49:12 +0200

    Error map

--- Mobishare.App/Areas/Admin/Pages/MapManagement/ManageCity.cshtml.cs
+++ Mobishare.App/Areas/Admin/Pages/MapManagement/ManageCity.cshtml.cs
@@ -4,9 +4,20 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace Mobishare.App.Areas.Admin.Pages.MapManagement
 {
     public class ManageCityModel : PageModel
+    {   
+    private readonly IConfiguration _configuration;
+
+    public string GoogleMapsApiKey { get; private set; }
+
+    public ManageCityModel(IConfiguration configuration)
     {
-        public void OnGet()
-        {
-        }
+        _configuration = configuration;
+    }
+
+    public void OnGet()
+    {
+        GoogleMapsApiKey = _configuration["GoogleMaps:ApiKey"]
+            ?? throw new InvalidOperationException("Google Maps API key is not configured.");
+    }
     }
 }
