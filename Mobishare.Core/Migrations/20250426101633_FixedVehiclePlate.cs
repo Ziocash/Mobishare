5a81891b478841d8e641696316c1ab3aead5bd76
BeastOfShadow <simone.negro.2002@gmail.com>
2025-04-26 16:22:17 +0200

    Fixed plate (not required)

---
+++ Mobishare.Core/Migrations/20250426101633_FixedVehiclePlate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mobishare.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixedVehiclePlate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "PricePerMinute",
+                table: "VehicleTypes",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "REAL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "PricePerMinute",
+                table: "VehicleTypes",
+                type: "REAL",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "TEXT");
+        }
+    }
+}
