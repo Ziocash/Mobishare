ba21ecc8e0782af89ccff4f5df9c967353dd0567
BeastOfShadow <simone.negro.2002@gmail.com>
2025-04-27 15:42:17 +0200

    Added query

--- Mobishare.Core/Requests/MappingProfile.cs
+++ Mobishare.Core/Requests/MappingProfile.cs
@@ -1,5 +1,7 @@
 using AutoMapper;
+using Mobishare.Core.Models.Maps;
 using Mobishare.Core.Models.Vehicles;
+using Mobishare.Core.Requests.Maps.CityRequests.Commands;
 using Mobishare.Core.Requests.Vehicles.VehicleRequests.Commands;
 using Mobishare.Core.Requests.Vehicles.VehicleTypeRequests.Commands;
 
@@ -9,6 +11,10 @@ public class MappingProfile : Profile
 {
     public MappingProfile()
     {
+        CreateMap<CreateCity, City>().ReverseMap();
+        // CreateMap<UpdateCity, City>().ReverseMap();
+        // CreateMap<DeleteCity, City>().ReverseMap();
+
         CreateMap<CreateVehicle, Vehicle>().ReverseMap();
         // CreateMap<UpdateVehicle, Vehicle>().ReverseMap();
         // CreateMap<DeleteVehicle, Vehicle>().ReverseMap();
