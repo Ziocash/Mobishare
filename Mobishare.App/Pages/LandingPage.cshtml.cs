38d89b6245258c8328df6cdd15bf448755fd7019
BeastOfShadow <simone.negro.2002@gmail.com>
2025-05-21 16:10:07 +0200

    Dynamic card added

--- Mobishare.App/Pages/LandingPage.cshtml.cs
+++ Mobishare.App/Pages/LandingPage.cshtml.cs
@@ -1,12 +1,55 @@
-using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
+using MediatR;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Mobishare.App.Services;
+using Mobishare.Core.Requests.Vehicles.RideRequests.Queries;
+using Mobishare.Core.UiModels;
 
 namespace Mobishare.App.Pages
 {
-    public class LangingPageModel : PageModel
+    public class LandingPageModel : PageModel
     {
-        public void OnGet()
+        private readonly ILogger<LandingPageModel> _logger;
+        private readonly IMediator _mediator;
+        private readonly IMapper _mapper;
+        private readonly IConfiguration _configuration;
+        private readonly IGoogleGeocodingService _googleGeocoding;
+
+        public List<RideDisplayInfo> AllRides { get; set; } = [];
+
+        public LandingPageModel(
+            ILogger<LandingPageModel> logger,
+            IMediator mediator,
+            IMapper mapper,
+            IConfiguration configuration,
+            IGoogleGeocodingService googleGeocoding)
         {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+            _googleGeocoding = googleGeocoding ?? throw new ArgumentNullException(nameof(googleGeocoding));
+        }
+
+        public async Task OnGet()
+        {
+            var rides = await _mediator.Send(new GetAllRides());
+
+            foreach (var ride in rides)
+            {
+                var rideInfo = new RideDisplayInfo { Ride = ride };
+
+                if (ride.PositionEnd != null)
+                {
+                    _logger.LogDebug("Location end: lat={Lat}, lon={Lon}", ride.PositionEnd.Latitude, ride.PositionEnd.Longitude);
+                    rideInfo.EndLocationName = await _googleGeocoding.GetAddressFromCoordinatesAsync((double)ride.PositionEnd.Latitude, (double)ride.PositionEnd.Longitude);
+
+                    _logger.LogDebug("Address result: {Address}", rideInfo.EndLocationName);
+                } 
+                if (ride.PositionStart != null) rideInfo.StartLocationName = await _googleGeocoding.GetAddressFromCoordinatesAsync((double)ride.PositionStart.Latitude, (double)ride.PositionStart.Longitude);
+
+                AllRides.Add(rideInfo);
+            }
         }
     }
 }
