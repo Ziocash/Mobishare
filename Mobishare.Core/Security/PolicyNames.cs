753fa85400b112e3ec847b9bec734cb644f11f15
Daniele-cosimo <cosimodaniele70@gmail.com>
2025-05-23 16:12:34 +0200

    Created policy Name

--- Mobishare.Core/Security/PolicyNames.cs
+++ Mobishare.Core/Security/PolicyNames.cs
@@ -5,7 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Mobishare.Core.Security;
+
 public static class PolicyNames
 {
 	public const string IsAdmin = "IsAdmin";
+	public const string IsStaff = "IsStaff";
+	public const string IsTechnician = "IsTechnician";
 }
