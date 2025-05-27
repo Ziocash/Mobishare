37dbb232c1de73e8f29a57a491835b988b2d0197
Daniele-cosimo <cosimodaniele70@gmail.com>
2025-05-27 23:02:25 +0200

    new table for Balance history

---
+++ Mobishare.Core/Models/UserRelated/HistoryCredit.cs
@@ -0,0 +1,16 @@
+using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace Mobishare.Core.Models.UserRelated;
+
+public class HistoryCredit
+{
+    public int Id { get; set; }
+    public double Credit { get; set; }
+    public IdentityUser User {get; set;}
+    public string UserId { get; set; }
+    public Balance Balance { get; set; }
+    public int BalanceId { get; set; }
+    public string TransactionType { get; set; } 
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
