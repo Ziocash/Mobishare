01b15704b3f2e8645f00da4418f14c4cd605f261
Daniele-cosimo <cosimodaniele70@gmail.com>
2025-05-27 23:02:56 +0200

    modelConfiguration for history Table

---
+++ Mobishare.Core/ModelsConfigurations/UserRelated/HistoryCreditConfiguration.cs
@@ -0,0 +1,36 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Mobishare.Core.Models.UserRelated;
+
+namespace Mobishare.Core.ModelsConfigurations.UserRelated;
+
+public class HistoryCreditConfiguration : IEntityTypeConfiguration<HistoryCredit>
+{
+    public void Configure(EntityTypeBuilder<HistoryCredit> builder)
+    {
+        builder.ToTable("HistoryCredits");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Credit)
+            .IsRequired();
+
+        builder.Property(x => x.CreatedAt)
+            .IsRequired()
+            .HasDefaultValue(DateTime.UtcNow);
+
+        builder.HasOne(x => x.User)
+               .WithMany()
+               .HasForeignKey(x => x.UserId)
+               .IsRequired();
+               
+        builder.HasOne(x => x.Balance)
+                .WithMany()
+                .HasForeignKey(x => x.BalanceId)
+                .IsRequired();
+
+        builder.Property(x => x.TransactionType)
+                .IsRequired();
+    }      
+}
