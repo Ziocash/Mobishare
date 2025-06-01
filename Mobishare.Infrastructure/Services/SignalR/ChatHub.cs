d12f6214a2c25dc69dd28e8ecf4a75008ff19f5a
BeastOfShadow <simone.negro.2002@gmail.com>
2025-06-02 01:45:34 +0200

    Implemented services and logics
    Co-Authored-By: Daniele-cosimo <32614957+The-Forest03@users.noreply.github.com>

--- Mobishare.Infrastructure/Services/SignalR/ChatHub.cs
+++ Mobishare.Infrastructure/Services/SignalR/ChatHub.cs
@@ -1,24 +1,41 @@
+using Microsoft.Extensions.Configuration;
 using AutoMapper;
 using Ganss.Xss;
-using Markdig;
 using MediatR;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Logging;
+using Mobishare.Core.Enums.Ai;
 using Mobishare.Core.Enums.Chat;
 using Mobishare.Core.Models.Chats;
 using Mobishare.Core.Requests.Chats.ChatMessageRequests.Commands;
+using Mobishare.Core.Requests.Chats.MessagePairRequests.Commands;
 using Mobishare.Infrastructure.Services.ChatBotAIService;
-using System.Diagnostics;
+using OllamaSharp;
+using System.Text.Json;
 
 public class ChatHub : Hub
 {
     private readonly IOllamaService _ollamaService;
+    private readonly IConfiguration _configuration;
     private readonly IMediator _mediatr;
     private readonly IMapper _mapper;
     private readonly ILogger<ChatHub> _logger;
+    private readonly IEmbeddingService _embeddingService;
+    private readonly IKnowledgeBaseRetriever _knowledgeBaseRetriever;
 
-    public ChatHub(IOllamaService ollamaService, IMediator mediator, IMapper mapper, ILogger<ChatHub> logger)
+    public ChatHub
+    (
+        IOllamaService ollamaService,
+        IMediator mediator, IMapper mapper,
+        ILogger<ChatHub> logger,
+        IConfiguration configuration,
+        IEmbeddingService embeddingService,
+        IKnowledgeBaseRetriever knowledgeBaseRetriever
+    )
     {
+        _knowledgeBaseRetriever = knowledgeBaseRetriever ?? throw new ArgumentNullException(nameof(knowledgeBaseRetriever));
+        _embeddingService = embeddingService ?? throw new ArgumentNullException(nameof(embeddingService));
+        _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
         _ollamaService = ollamaService ?? throw new ArgumentNullException(nameof(ollamaService));
         _mediatr = mediator ?? throw new ArgumentNullException(nameof(mediator));
         _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
@@ -27,17 +44,28 @@ public class ChatHub : Hub
 
     public async Task SendMessage(string conversationId, string message)
     {
+        ChatMessage aiResponse;
+        bool answered = false;
         var sanitizer = new HtmlSanitizer();
         message = sanitizer.Sanitize(message);
+
         _logger.LogInformation($"Ricevuto messaggio: {message} nella conversazione {conversationId}");
-        var response = await _mediatr.Send(new CreateChatMessage {
+        var userEmbedResponse = await _embeddingService.CreateEmbeddingAsync(message);
+        var deserializeEmbedResponse = JsonSerializer.Serialize(userEmbedResponse);
+
+        var userResponse = await _mediatr.Send(new CreateChatMessage
+        {
             ConversationId = int.Parse(conversationId),
             Message = message,
             Sender = MessageSenderType.User.ToString(),
-            CreatedAt = DateTime.UtcNow
-        });
-        
+            CreatedAt = DateTime.UtcNow,
+            Embedding = deserializeEmbedResponse
+        }); 
+
         Console.WriteLine($"Messaggio ricevuto da te: {message}");
+
+        // TODO: da mettere nel prompt
+        var relevantMessages = await _knowledgeBaseRetriever.GetRelevantPairsAsync(userEmbedResponse, 3);
 
         try
         {
@@ -53,24 +81,44 @@ public class ChatHub : Hub
             }
 
             completeResponse = sanitizer.Sanitize(completeResponse);
-            
-            var aiResponse = await _mediatr.Send(new CreateChatMessage
+
+            var aiEmbedResponse = await _embeddingService.CreateEmbeddingAsync(completeResponse);
+            var deserializeAiEmbedResponse = JsonSerializer.Serialize(aiEmbedResponse);
+
+            aiResponse = await _mediatr.Send(new CreateChatMessage
             {
                 ConversationId = int.Parse(conversationId),
                 Message = completeResponse,
                 Sender = MessageSenderType.AiAgent.ToString(),
-                CreatedAt = DateTime.UtcNow
+                CreatedAt = DateTime.UtcNow,
+                Embedding = deserializeAiEmbedResponse
             });
+
+            answered = true;
         }
         catch (Exception ex)
         {
             _logger.LogError($"Errore interno: {ex.Message}");
             await Clients.Caller.SendAsync("ReceiveMessage", "MobishareBot", "An internal error has occurred.", DateTime.UtcNow.ToLocalTime().ToString("g"));
-            var aiResponse = await _mediatr.Send(new CreateChatMessage {
+            aiResponse = await _mediatr.Send(new CreateChatMessage
+            {
                 ConversationId = int.Parse(conversationId),
                 Message = "An internal error has occurred.",
                 Sender = MessageSenderType.AiAgent.ToString(),
                 CreatedAt = DateTime.UtcNow
+            });
+        }
+        
+        if (userResponse != null && aiResponse != null)
+        {
+            var messagePair = await _mediatr.Send(new CreateMessagePair
+            {
+                UserMessageId = userResponse.Id,
+                AiMessageId = aiResponse.Id,
+                IsForRag = true, // TODO: cambiare a FALSE!
+                SourceType = SourceType.Chatbot.ToString(),
+                Answered = answered,
+                Language = _configuration["Ollama:Llm:ModelName"] ?? "default"
             });
         }
     }
