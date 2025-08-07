982c69b6c0f63bd814ca5ac304e64ecabbb9778d
BeastOfShadow <simone.negro.2002@gmail.com>
2025-08-07 11:13:28 +0200

    Refactor visualizing map to use HTTP API
    Replaced mediator and mapper usage in VisualizingMap page model with direct HTTP API calls for API'S operations.

--- Mobishare.App/Areas/Admin/Pages/MapManagement/VisualizeMap.cshtml.cs
+++ Mobishare.App/Areas/Admin/Pages/MapManagement/VisualizeMap.cshtml.cs
@@ -1,12 +1,7 @@
-using AutoMapper;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Mobishare.Core.Models.Maps;
-using Mobishare.Core.Requests.Maps.CityRequests.Queries;
-using Mobishare.Core.Requests.Maps.ParkingSlotRequests.Queries;
 using Mobishare.Core.Security;
 
 namespace Mobishare.App.Areas.Admin.Pages.MapManagement
@@ -15,25 +10,34 @@ namespace Mobishare.App.Areas.Admin.Pages.MapManagement
     public class VisualizeMapModel : PageModel
     {
         private readonly ILogger<VisualizeMapModel> _logger;
-        private readonly IMediator _mediator;
+        private readonly HttpClient _httpClient;
         public IEnumerable<ParkingSlot> AllParkingSlots { get; set; }
         public IEnumerable<City> AllCities { get; set; }
         public string AllParkingSlotsPerimeter { get; set; }
         public string AllCitiesPerimeter { get; set; }
 
-        public VisualizeMapModel(ILogger<VisualizeMapModel> logger, IMediator mediator)
+        public VisualizeMapModel(ILogger<VisualizeMapModel> logger, IHttpClientFactory httpClientFactory)
         {
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
-            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+            _httpClient = httpClientFactory.CreateClient("CityApi");
         }
 
         public async Task OnGet()
         {
-            AllCities = await _mediator.Send(new GetAllCities());
-            foreach (var city in AllCities) AllCitiesPerimeter += city.PerimeterLocation + ";";
+            try
+            {
+                var response = await _httpClient.GetFromJsonAsync<IEnumerable<City>>("api/City/AllCities");
+                AllCities = response ?? [];
+                var parkigSlotResponse = await _httpClient.GetFromJsonAsync<IEnumerable<ParkingSlot>>("api/ParkingSlot/AllParkingSlots");
+                AllParkingSlots = parkigSlotResponse ?? [];
 
-            AllParkingSlots = await _mediator.Send(new GetAllParkingSlots());
-            foreach (var parkingSlot in AllParkingSlots) AllParkingSlotsPerimeter += parkingSlot.PerimeterLocation + ";";
+                AllCitiesPerimeter = string.Join(";", AllCities.Select(c => c.PerimeterLocation));
+                AllParkingSlotsPerimeter = string.Join(";", AllParkingSlots.Select(p => p.PerimeterLocation));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error loading data");
+            }
         }
     }
 }
