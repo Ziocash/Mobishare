e7583b787e55b9f93b5a68893a5fc47210aa7d28
BeastOfShadow <simone.negro.2002@gmail.com>
2025-09-08 14:25:54 +0200

    Display all reports for admin ticket management
    Added logic to fetch and display all reports for the current user in the ManageTickets admin page. The page now retrieves reports via an API call using the user's ID and renders their descriptions in the view.

--- Mobishare.App/Areas/Admin/Pages/Reports/ManageTickets.cshtml.cs
+++ Mobishare.App/Areas/Admin/Pages/Reports/ManageTickets.cshtml.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Mobishare.Core.Models.Vehicles;
 using Mobishare.Core.Security;
 
 namespace MyApp.Namespace
@@ -8,8 +10,32 @@ namespace MyApp.Namespace
     [Authorize(Policy = PolicyNames.IsTechnician)]
     public class ManageTicketsModel : PageModel
     {
-        public void OnGet()
+        private readonly ILogger<ManageTicketsModel> _logger;
+        private readonly HttpClient _httpClient;
+        private readonly UserManager<IdentityUser> _userManager;
+        public IEnumerable<Report> AllReports { get; set; }
+
+        public ManageTicketsModel(
+            ILogger<ManageTicketsModel> logger,
+            IHttpClientFactory httpClientFactory,
+            UserManager<IdentityUser> userManager)
         {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _httpClient = httpClientFactory.CreateClient("CityApi");
+            _userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
+        }
+
+        public async Task OnGetAsync()
+        {
+            var userId = _userManager.GetUserId(User);
+
+            if (userId == null)
+            {
+                _logger.LogWarning("User ID is null.");
+            } else {
+                var response = await _httpClient.GetFromJsonAsync<IEnumerable<Report>>($"api/Report/AllReports/{userId}");
+                AllReports = response ?? [];
+            }
         }
     }
 }
