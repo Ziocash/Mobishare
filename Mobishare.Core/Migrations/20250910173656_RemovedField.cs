0efdaffe2cf2bc3105ad13ffdfd3d128f666da3e
BeastOfShadow <simone.negro.2002@gmail.com>
2025-09-10 19:58:33 +0200

    Changed model

---
+++ Mobishare.Core/Migrations/20250910173656_RemovedField.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mobishare.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovedField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AssignedAt",
+                table: "Reports");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "AssignedAt",
+                table: "Reports",
+                type: "TEXT",
+                nullable: true);
+        }
+    }
+}
