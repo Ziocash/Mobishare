3d71afc23b2e34d50e6335aef8991820198cc9cc
BeastOfShadow <simone.negro.2002@gmail.com>
2025-09-11 16:09:31 +0200

    Co-authored-by: Daniele-cosimo <The-Forest03@users.noreply.github.com> Co-authored-by: Matteo Schintu <SkennyCMD@users.noreply.github.com>

--- Mobishare.Ai/ChatBotAIService/ToolExecutor/Tools/VehicleTools/VehicleTool.cs
+++ Mobishare.Ai/ChatBotAIService/ToolExecutor/Tools/VehicleTools/VehicleTool.cs
@@ -8,16 +8,17 @@ namespace Mobishare.Ai.ChatBotAIService.ToolExecutor.Tools.VehicleTools;
 
 public class VehicleTool : IVehicleTool
 {
-    private readonly IMapper _mapper;
-    private readonly IMediator _mediatr;
+    private readonly HttpClient _httpClient;
     private readonly ILogger<VehicleTool> _logger;
     private readonly IUserContextService _userContext;
     public string? UserId { get; set; }
 
-    public VehicleTool(IMapper mapper, IMediator mediatr, ILogger<VehicleTool> logger, IUserContextService userContext)
+    public VehicleTool(
+        IHttpClientFactory httpClientFactory,
+        ILogger<VehicleTool> logger,
+        IUserContextService userContext)
     {
-        _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
-        _mediatr = mediatr ?? throw new ArgumentNullException(nameof(mediatr));
+        _httpClient = httpClientFactory.CreateClient("CityApi");
         _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         _userContext = userContext ?? throw new ArgumentNullException(nameof(userContext));
     }
@@ -25,7 +26,7 @@ public class VehicleTool : IVehicleTool
     
     public Task<string> ReportVehicleIssueAsync(string description)
     {
-        // implementa con il db
+        
         // _logger.LogInformation("Segnalazione veicolo ricevuta: {description}", description);
         return Task.FromResult($"Segnalazione ricevuta: {description}");
     }
